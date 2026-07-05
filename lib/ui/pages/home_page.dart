8ca727e7b012f0049a45f9cab4ddf9aa1f2b1fa5
soekhant01 <soekhant.dev@gmail.com>
2026-07-05 11:20:18 +0630

    Cupertino style navigation bar implemented

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.book), activeIcon: Icon(CupertinoIcons.book_fill), label: "Dictionary"),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.settings), activeIcon: Icon(CupertinoIcons.settings_solid), label: "Setting")
+
+      ]),
+      tabBuilder: (_, index) {
+        return Center();
+      },
+    );
+  }
+}
